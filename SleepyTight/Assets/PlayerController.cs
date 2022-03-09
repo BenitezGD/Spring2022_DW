2996c58075f3721c6015a8d002debe1bea3de939
Humberto Benitez <benitezh@sheridancollege.ca>
2022-03-08 19:47:44 -0500

    Player controller updated and added the ability to move moveable objects

--- SleepyTight/Assets/PlayerController.cs
+++ SleepyTight/Assets/PlayerController.cs
@@ -5,34 +5,90 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5;
-
+    public float speed = 20;
+    float h = 0;
+    float v = 0;
     Rigidbody2D rb;
+    SpriteRenderer spr;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        if ((h == 1 || h ==-1) && (v == 1 || v == -1))
+        /*
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(h * speed/2 * Time.deltaTime, v * speed/2 * Time.deltaTime);
+            rb.AddForce(Vector2.up * speed * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector2.down * speed * Time.deltaTime, ForceMode2D.Impulse);
         }
 
         else
         {
-            transform.position += new Vector3(h * speed * Time.deltaTime, v * speed * Time.deltaTime);
+            rb.velocity = new Vector2(rb.velocity.x, 0);
         }
 
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * speed * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector2.right * speed * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        Mathf.Clamp(rb.velocity.magnitude,0f, speed);
+        */
+
+        h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position += new Vector3(h, v );
+
+        if(h < 0 )
+        {
+            spr.flipX = true;
+        }
+
+        else if (h > 0)
+        {
+            spr.flipX = false;
+        }
         
-        Debug.Log(rb.velocity.magnitude);
-        //Mathf.Clamp(rb.velocity.magnitude,0f, speed);
+
     }
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Moveable")
+        {
+            if(Input.GetKey(KeyCode.E))
+            {
+                other.transform.parent = transform;
+                
+            }
+
+            else
+            {
+                other.transform.parent = null;
+            }
+        }
+    }
+
 }
