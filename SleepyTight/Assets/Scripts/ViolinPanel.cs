ebeb0de9afa0925686b41749829499c6c2ce7d27
Humberto Benitez <benitezh@sheridancollege.ca>
2022-03-09 19:22:26 -0500

    ScrollBar1 Working

--- SleepyTight/Assets/Scripts/ViolinPanel.cs
+++ SleepyTight/Assets/Scripts/ViolinPanel.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ViolinPanel : MonoBehaviour
 {
     CanvasGroup canvasGroup;
 
+    public Scrollbar scroll1;
+    public Image knob1Correct;
 
     //Check if the player has the Violin Cables to be able to use the panel
     public bool violinCable = false;
+    public bool scroll1Position = false;
 
     private float startPosX;
 
@@ -23,6 +27,14 @@ public class ViolinPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (scroll1.value > 0.75 && scroll1.value < 0.90)
+        {
+            knob1Correct.gameObject.SetActive(true);
+        }
+
+        else
+        {
+            knob1Correct.gameObject.SetActive(false);
+        }
     }
 }
