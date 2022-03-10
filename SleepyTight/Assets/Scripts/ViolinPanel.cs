59de66a0c9b4468348fe0258611927ad17c56ead
Tiago Massochin <zizomass@outlook.com>
2022-03-09 19:05:05 -0500

    ui: room 1 interface
    The Violin Panel and the Violin Tune canvas objects are now add to the room 1 scene

---
+++ SleepyTight/Assets/Scripts/ViolinPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViolinPanel : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+
+
+    //Check if the player has the Violin Cables to be able to use the panel
+    public bool violinCable = false;
+
+    private float startPosX;
+
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
