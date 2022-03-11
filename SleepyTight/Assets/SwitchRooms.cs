2caa7ac5198a71e23d4cc60b0d9f2a111f2bc3d2
Humberto Benitez <benitezh@sheridancollege.ca>
2022-03-10 19:50:39 -0500

    Made the 6 rooms look similar with the room change functionality

--- SleepyTight/Assets/SwitchRooms.cs
+++ SleepyTight/Assets/SwitchRooms.cs
@@ -25,5 +25,30 @@ public class SwitchRooms : MonoBehaviour
         {
             SceneManager.LoadScene("Room_2");
         }
+
+        if (sceneNum == 2 && other.tag == "Player")
+        {
+            SceneManager.LoadScene("Room_3");
+        }
+
+        if (sceneNum == 3 && other.tag == "Player")
+        {
+            SceneManager.LoadScene("Room_4");
+        }
+
+        if (sceneNum == 4 && other.tag == "Player")
+        {
+            SceneManager.LoadScene("Room_5");
+        }
+
+        if (sceneNum == 5 && other.tag == "Player")
+        {
+            SceneManager.LoadScene("Room_6");
+        }
+
+        if (sceneNum == 6 && other.tag == "Player")
+        {
+            SceneManager.LoadScene("Room_1");
+        }
     }
 }
